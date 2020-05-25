662e02c9bd3d9e357191204af076aad120be4322
dmlgus <dmlgus1222@naver.com>
2020-05-25 13:07:33 +0900

    random

---
+++ Java_06/src/com/biz/hello/Var_01answer.java
@@ -0,0 +1,20 @@
+package com.biz.hello;
+
+import java.util.Random;
+
+public class Var_01answer {
+
+	public static void main(String[] args) {
+		
+		Random rnd = new Random();
+		int num = rnd.nextInt(100)+1;
+		
+		//내꺼랑 답 똑같움
+		
+		
+		
+		
+		
+		
+	}
+}
