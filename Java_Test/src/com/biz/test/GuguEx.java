d9f5ccb7d410f1068a6f16786e570861bc3ad731
lovelyhyeony <dmlgus1222@naver.com>
2020-11-02 11:50:05 +0900

    1102

---
+++ Java_Test/src/com/biz/test/GuguEx.java
@@ -0,0 +1,15 @@
+package com.biz.test;
+
+
+public class GuguEx {
+
+	public static void main(String[] args) {
+		int num1 = 7;
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.printf("%d x %d = %d\n",num1,(i+1),(num1 *(i+1)));
+		}
+		
+		
+	}
+}
