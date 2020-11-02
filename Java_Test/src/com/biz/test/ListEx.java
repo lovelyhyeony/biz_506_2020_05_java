440f7ff04b676d36db5e209bb08639a82952dfa8
lovelyhyeony <dmlgus1222@naver.com>
2020-11-02 15:14:14 +0900

    1102

---
+++ Java_Test/src/com/biz/test/ListEx.java
@@ -0,0 +1,13 @@
+package com.biz.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListEx {
+
+	// List형은 클래스를 제네릭으로 포함을 한다.
+	// List에는 Primitive형은 포함할 수 없다.
+	// List<int> << 할수없다.
+	List<Integer> intList = new ArrayList<Integer>();
+	
+}
